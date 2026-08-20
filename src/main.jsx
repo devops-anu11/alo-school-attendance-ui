b4640c1dc742e109cfd6597be2300527951d1b21
devops-anu11 <devopsalogroups@gmail.com>
2026-08-20 19:15:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/theme.css';
-import App from './App';
+import App from './App.jsx';
 // import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
